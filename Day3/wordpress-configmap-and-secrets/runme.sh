ec1e3693cf3768ab6d8ce0b9702f4754e566af9c
Jeganathan Swaminathan <mail2jegan@gmail.com>
2023-11-29 17:58:19 +0530

    Added Day3 lab exercises.

---
+++ Day3/wordpress-configmap-and-secrets/runme.sh
@@ -0,0 +1,12 @@
+oc apply -f wordpress-secrets.yml
+oc apply -f wordpress-cm.yml
+oc apply -f mysql-pv.yml
+oc apply -f mysql-pvc.yml
+oc apply -f mysql-deploy.yml
+oc apply -f mysql-svc.yml
+
+oc apply -f wordpress-pv.yml
+oc apply -f wordpress-pvc.yml
+oc apply -f wordpress-deploy.yml
+oc apply -f wordpress-svc.yml
+oc apply -f wordpress-route.yml
